212c23e54d79acad9ba40c85006737a5f93e580e
Ankur srivastava <ankursrivastava2070@gmail.com>
2023-06-05 17:14:58 +0530

    download on click event added

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Grid = ({ photos }) => {
+  const handleClick = async (event) => {
+    const imageUrl = event.target.src;
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = imageUrl.substring(imageUrl.lastIndexOf('/') + 1);
+    link.click();
+  };
   return (
     <>
       <h1>My Gallery</h1>
@@ -10,7 +20,7 @@ const Grid = ({ photos }) => {
             <img
               src={`https://photo-j7r5.onrender.com/uploads/${photo}`}
               alt="grid_image"
-            />
+              onClick={handleClick}/ >
           </div>
         ))}
       </div>
